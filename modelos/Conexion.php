842d851c3d9b82d71b9cf9b7f7bee2ef4adb5152
“Castellanos“ <rcastellanos11383@gmail.com>
2024-06-08 11:20:19 -0600

    CONEXION A LA BD

---
+++ modelos/Conexion.php
@@ -0,0 +1,25 @@
+<?php
+
+abstract class Conexion{
+    protected static $conexion = null;
+
+    public static function connectar() : PDO{
+        if(!self::$conexion){
+
+            try {
+                self::$conexion = new PDO("informix:host=host.docker.internal; service=9088;database=cia_programadores; server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix", "in4mix");
+                self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+            } catch (PDOException $e) {
+                echo "No hay conexion a la BD";
+                echo "<br>";
+                echo $e->getMessage();
+                self::$conexion = null;
+                return null;
+                exit;
+            }
+        }
+
+        return self::$conexion;
+    }
+}
